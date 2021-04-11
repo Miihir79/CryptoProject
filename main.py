6a514fd2f7dc17c83dd030a0cdac1772335b9a89
Miihir79 <mihirrshah02@gmail.com>
2021-04-11 22:10:25 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,120 @@
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+
+# your api here
+API_KEY = "your api key"
+
+
+def getYearlyrates(amount, currency, converted_currency, days):
+    today_date = datetime.datetime.now()
+    # Api takes only days so converting into days
+    date_yearAgo = (today_date - datetime.timedelta(days=1 * days))
+
+    url = f'https://api.exchangerate.host/timeseries'
+    payload = {'base': currency, 'amount': amount, 'start_date': date_yearAgo.date(), 'end_date': today_date.date()}
+
+    response_currency = requests.get(url, params=payload)
+    data = response_currency.json()
+    currency_hist = {}
+    rates_history = []
+
+    for item in data['rates']:
+        current_date = item
+        currecy_rate = data['rates'][item][converted_currency]
+
+        currency_hist[current_date] = [currecy_rate]
+        rates_history.append(currecy_rate)
+
+    pd_data = pd.DataFrame(currency_hist).transpose()
+    pd_data.columns = ['Rate']
+    pd.set_option('display.max_rows', None)
+    print(pd_data)
+
+    plt.plot(rates_history)
+    plt.ylabel(f'{amount} {currency} to {converted_currency}')
+    plt.xlabel('Days')
+    plt.title(f'Current exchange rate for {amount} {currency} to {converted_currency} is {rates_history[-1]}')
+    plt.show()
+
+
+def get_crypto_data(currencyFun="USD", crypto="BTC", invert='true'):
+    url = f'https://rest.coinapi.io/v1/exchangerate/{currencyFun}?invert={invert}'
+    headers = {'X-CoinAPI-Key': API_KEY}
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    assets_names, assets_rates = [], []
+
+    for asset in data['rates']:
+        asset_id = asset['asset_id_quote']
+        rate = asset['rate']
+
+        assets_names.append(asset_id)
+        assets_rates.append(rate)
+
+    raw_data = {
+        'assets': assets_names,
+        'rates': assets_rates
+    }
+    df = pd.DataFrame(raw_data)
+    pd.set_option('display.max_rows', None)
+
+    value = df.loc[df.assets == crypto, 'rates'].tolist()[0]
+    print(df)
+    return value
+
+
+# Execution starts here
+
+want_to_continu = 1
+while want_to_continu == 1:
+    whatToDO = int(
+        input('\nPlease Enter digit for what you want to do: \n 1->Currency Conversions \n 2->Crypto Price track \n '
+              '3->Exit'))
+    if whatToDO == 1:
+        graphChahiye = int(
+            input('Please enter what you want to do with currencies: \n 1-> Check for conversions \n 2->Track its '
+                  'changes '))
+        if graphChahiye == 1:
+
+            CurrencyFrom = input('Enter the abbr of the currency to be converted:')
+            CurrencyTo = input('Enter the abbr of currency to be checked:')
+            money = input('Enter the amount to be converted:')
+            getYearlyrates(money, CurrencyFrom, CurrencyTo, 1)
+
+        elif graphChahiye == 2:
+
+            CurrencyFrom = input('Enter the abbr of the currency to be converted:')
+            CurrencyTo = input('Enter the abbr of currency to be checked:')
+            daysTocheck = int(input('Enter the days to be checked(Max 1year):'))
+            getYearlyrates(1, CurrencyFrom, CurrencyTo, daysTocheck)
+
+        else:
+
+            print("\nEnter a valid response")
+
+    elif whatToDO == 2:
+
+        invert_checker = int(input(' Enter What has to be done \n1-> Crypto -> Currency \n2-> Currency -> Crypto'))
+        Crypto = input('Enter the crypto currency abbreviation:')
+        currency_entered = input('Enter the currency abbreviation:')
+
+        if invert_checker == 1:
+
+            valueToPrint = get_crypto_data(currency_entered, Crypto)
+            print(f'\n The current price of 1 {Crypto} is {valueToPrint} in {currency_entered}')
+
+        elif invert_checker == 2:
+
+            valueToPrint = get_crypto_data(currency_entered, Crypto, 'false')
+            print(f'\n The current price of 1 {currency_entered} is {valueToPrint} in {Crypto}')
+            
+        else:
+            print('\nEnter a valid response')
+
+    elif whatToDO == 3:
+        break
+    else:
+        print("\nEnter a valid response")
